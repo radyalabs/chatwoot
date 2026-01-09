c06c5c4d5eb601333e9630ef2692cc25139758a6
iwanlaudin <iwan@radyalabs.com>
2026-01-10 05:24:11 +0700

    fix(whatsapp-unofficial): update send_message signature in base service
    - Simplify method to accept single message parameter
    - Phone number is now handled within message object

--- app/services/whatsapp_unofficial/providers/base_service.rb
+++ app/services/whatsapp_unofficial/providers/base_service.rb
@@ -1,7 +1,7 @@
 class WhatsappUnofficial::Providers::BaseService
   pattr_initialize [:whatsapp_channel!]
 
-  def send_message(_phone_number, _message)
+  def send_message(_message)
     raise 'Overwrite this method in child class'
   end
 end

d32e8e00adc91c6054f57f671923076eb9c9981c
octovalents <68523239+octovalents@users.noreply.github.com>
2026-03-25 16:11:06 +0700

    feat: enforce active subscription check on all account API endpoints

--- app/controllers/api/v1/accounts/base_controller.rb
+++ app/controllers/api/v1/accounts/base_controller.rb
@@ -2,5 +2,17 @@ class Api::V1::Accounts::BaseController < Api::BaseController
   include SwitchLocale
   include EnsureCurrentAccountHelper
   before_action :current_account
+  before_action :ensure_active_subscription
   around_action :switch_locale_using_account_locale
+
+  private
+
+  def ensure_active_subscription
+    subscription = current_account&.active_subscription
+    render_subscription_expired unless subscription&.active?
+  end
+
+  def render_subscription_expired
+    render json: { error: 'subscription_expired', message: 'Your account subscription has expired' }, status: :forbidden
+  end
 end

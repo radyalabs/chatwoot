8923327bfdf06ea6ef72f7f60307c912bea3dbc2
octovalents <68523239+octovalents@users.noreply.github.com>
2026-04-07 11:26:55 +0700

    fix: add signature file attachment and current conversation id

--- app/javascript/dashboard/components-next/NewConversation/components/ActionButtons.vue
+++ app/javascript/dashboard/components-next/NewConversation/components/ActionButtons.vue
@@ -177,7 +177,7 @@ useKeyboardEvents(keyboardEvents);
         />
       </div>
       <FileUpload
-        v-if="hasSelectedInbox && !isWhatsappInbox && !hasNoInbox"
+        v-if="!isWhatsappInbox && !hasNoInbox"
         ref="uploadAttachment"
         input-id="composeNewConversationAttachment"
         :size="4096 * 4096"

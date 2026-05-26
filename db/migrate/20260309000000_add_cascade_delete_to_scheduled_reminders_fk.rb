8123c3440a7a951ce181fa74edf29672ef93ec11
iwanlaudin <iwan@radyalabs.com>
2026-05-26 14:43:20 +0700

    Add if_exists checks before removing foreign keys and ignore .env.test.

--- db/migrate/20260309000000_add_cascade_delete_to_scheduled_reminders_fk.rb
+++ db/migrate/20260309000000_add_cascade_delete_to_scheduled_reminders_fk.rb
@@ -1,12 +1,12 @@
 class AddCascadeDeleteToScheduledRemindersFk < ActiveRecord::Migration[7.0]
   def change
-    remove_foreign_key :scheduled_reminders, :inboxes
+    remove_foreign_key :scheduled_reminders, :inboxes, if_exists: true
     add_foreign_key :scheduled_reminders, :inboxes, on_delete: :cascade
 
-    remove_foreign_key :scheduled_reminders, :accounts
+    remove_foreign_key :scheduled_reminders, :accounts, if_exists: true
     add_foreign_key :scheduled_reminders, :accounts, on_delete: :cascade
 
-    remove_foreign_key :scheduled_reminders, :ai_agents
+    remove_foreign_key :scheduled_reminders, :ai_agents, if_exists: true
     add_foreign_key :scheduled_reminders, :ai_agents, on_delete: :cascade
   end
 end

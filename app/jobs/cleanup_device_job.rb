b90c65a02eb26f7dcba0acd1d6e6c71c93c73c0c
zafrandivac <zafrandivac38@gmail.com>
2026-06-09 16:42:21 +0700

    fix: soft delete not working

--- app/jobs/cleanup_device_job.rb
+++ app/jobs/cleanup_device_job.rb
@@ -2,17 +2,19 @@ class CleanupDeviceJob < ApplicationJob
   queue_as :low
 
   def perform(channel_id)
-    waha_service = Waha::WahaService.instance
-    waha_service.delete_device(device_id: channel_id)
+    begin
+      waha_service = Waha::WahaService.instance
+      waha_service.delete_device(device_id: channel_id)
+    rescue StandardError => e
+      Rails.logger.warn "API Gowa gagal (mungkin device sudah terhapus): #{e.message}"
+    end
 
     channel = Channel::WhatsappUnofficial.find_by(id: channel_id)
     
     if channel && !channel.phone_number.to_s.include?('_deleted_')
       new_phone_number = "#{channel.phone_number}_deleted_#{Time.current.to_i}"
-      channel.update_columns(phone_number: new_phone_number)
+      channel.update_columns(phone_number: new_phone_number, status: 'disconnected')
+      Rails.logger.info "Nomor #{channel.phone_number} berhasil dibebaskan di database."
     end
-
-  rescue StandardError => e
-    Rails.logger.error "Gagal memproses cleanup untuk channel #{channel_id}: #{e.message}"
   end
 end

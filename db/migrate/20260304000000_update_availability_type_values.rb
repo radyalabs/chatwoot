46f8a444aedac36b11e5b1fec7080652ede36a0f
iwanlaudin <iwan@radyalabs.com>
2026-07-03 15:15:38 +0700

    fix(db): avoid inbox model scope in availability migrations

--- db/migrate/20260304000000_update_availability_type_values.rb
+++ db/migrate/20260304000000_update_availability_type_values.rb
@@ -1,8 +1,8 @@
 class UpdateAvailabilityTypeValues < ActiveRecord::Migration[7.0]
   def up
     # Migrate existing data
-    Inbox.where(availability_type: 'turn_off_bot').update_all(availability_type: 'turn_on')
-    Inbox.where(availability_type: 'turn_off_channel').update_all(availability_type: 'turn_off')
+    execute("UPDATE inboxes SET availability_type = 'turn_on' WHERE availability_type = 'turn_off_bot'")
+    execute("UPDATE inboxes SET availability_type = 'turn_off' WHERE availability_type = 'turn_off_channel'")
 
     # Change default value
     change_column_default :inboxes, :availability_type, from: 'turn_off_bot', to: 'turn_on'
@@ -10,8 +10,8 @@ class UpdateAvailabilityTypeValues < ActiveRecord::Migration[7.0]
 
   def down
     # Rollback data
-    Inbox.where(availability_type: 'turn_on').update_all(availability_type: 'turn_off_bot')
-    Inbox.where(availability_type: 'turn_off').update_all(availability_type: 'turn_off_channel')
+    execute("UPDATE inboxes SET availability_type = 'turn_off_bot' WHERE availability_type = 'turn_on'")
+    execute("UPDATE inboxes SET availability_type = 'turn_off_channel' WHERE availability_type = 'turn_off'")
 
     # Revert default value
     change_column_default :inboxes, :availability_type, from: 'turn_on', to: 'turn_off_bot'

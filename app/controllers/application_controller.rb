f4649355212ada0145f9920520c7c220ee247a66
iwanlaudin <iwan@radyalabs.com>
2026-08-20 10:35:27 +0700

    feat(routes): add catch-all route for handling 404 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
   around_action :handle_with_exception, unless: :devise_controller?
 
+  def route_not_found
+    render plain: '404 Not Found', status: :not_found
+  end
+
   private
 
   def set_current_user
